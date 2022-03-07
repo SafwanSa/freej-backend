1929047c5934472f82dccfff7c7b8a6f5b44a3a9
Safwan <s.saigh@puritec.sa>
2022-03-07 22:33:13 +0300

    feat: implement campus general queries

---
+++ apps/campus/queries.py
@@ -0,0 +1,37 @@
+from .models import *
+from django.db.models import Q
+from typing import Iterable
+from core.errors import Error, APIError
+
+
+def get_all_campuses() -> Iterable[Campus]:
+    return Campus.objects.filter(is_deleted=False)
+
+
+def get_campus_by_id(id: int) -> Campus:
+    try:
+        return Campus.objects.get(id=id)
+    except Campus.DoesNotExist:
+        raise APIError(Error.INSTANCE_NOT_FOUND, extra=[Campus._meta.model_name])
+
+
+def get_campus_buildings(campus: Campus) -> Iterable[Building]:
+    return campus.buildings.filter(is_deleted=False)
+
+
+def get_building_by_id(id: int) -> Building:
+    try:
+        return Building.objects.get(id=id)
+    except Building.DoesNotExist:
+        raise APIError(Error.INSTANCE_NOT_FOUND, extra=[Building._meta.model_name])
+
+
+def get_building_rooms(building: Building) -> Iterable[Room]:
+    return building.rooms.filter(is_deleted=False)
+
+
+def get_room_by_id(id: int) -> Room:
+    try:
+        return Room.objects.get(id=id)
+    except Room.DoesNotExist:
+        raise APIError(Error.INSTANCE_NOT_FOUND, extra=[Room._meta.model_name])
--- core/errors.py
+++ core/errors.py
@@ -7,6 +7,18 @@ from enum import Enum
 class Error(Enum):
     DEFAULT = {"code": -100, "detail": _("Oops!. Something went wrong, please contact us")}
     _DEFAULT_MESSAGE = {"code": 0, "detail": _("Default: {}")}
+    INSTANCE_NOT_FOUND = {'code': -404, 'detail': _('{} not found!')}
+    INVALID_JWT_TOKEN = {'code': -100, 'detail': _('Invalid token!')}
+    REQUIRED_FIELD = {'code': 0, 'detail': _('This field is required!')}
+    DATA_IS_MISSING = {'code': -101, 'detail': _('Data is missing!')}
+    INVALID_TOKEN_OR_OTP = {'code': -460, 'detail': _("Invalid token or otp!")}
+    EXPIRED_TOKEN = {'code': -460, 'detail': _("Your token has expired!")}
+    NO_ACTIVE_ACCOUNT = {'code': -500, 'detail': _('No active account found with the given credentials!')}
+    BLOCKED_USER = {'code': -403, 'detail': _('User is banned!')}
+    # ACCOUNT_ALREADY_EXIST = {'code': -408, 'detail': _('Account with this mobile number or email already exist!')}
+    INVALID_OTP = {'code': -408, 'detail': _('OTP is invalid!')}
+    UNSUPPORTED_LANGUAGE = {'code': -487, 'detail': _('Provided language is unsupported!')}
+    NOT_ALLOWED_TO_REQUEST_OTP = {'code': -487, 'detail': _('You have already requested an OTP, please wait!')}
 
 
 class APIError(Exception):
