c48719cd3036be8abff85dd70b90a42bb09534da
Safwan <s.saigh@puritec.sa>
2022-03-12 03:57:17 +0300

    feat: implement register-related endpoints

--- apps/account/urls.py
+++ apps/account/urls.py
@@ -2,6 +2,9 @@ from django.urls import include, path
 from . import views
 
 urlpatterns = [
+    path('register/', views.RegisterView.as_view()),
+    path('register-confirmation/', views.ConfirmRegisterView.as_view()),
+
     path('access/', views.ResidentTokenObtainPairView.as_view()),
     path('refresh/', views.ResidentTokenRefreshView.as_view()),
 ]
--- apps/account/views.py
+++ apps/account/views.py
@@ -1,5 +1,6 @@
 from rest_framework.response import Response
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from apps.campus.services import ResidentService
 from .serializers import *
 from rest_framework.views import APIView
 from rest_framework.generics import ListAPIView
@@ -9,19 +10,27 @@ from .models import *
 from django.utils.translation import gettext_lazy as _
 from core.errors import Error, APIError
 from rest_framework.pagination import PageNumberPagination
-
-# class UserView(ListAPIView):
-#     queryset = User.objects.all()
-#     serializer_class = UserSerializer
-#     description = "This endpoint return the list of users"
+from .services import *
 
 
 class RegisterView(APIView):
-    pass
+    def post(self, request):
+        serializer = RegisterSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        otp = AccountService.register_resident(**serializer.data)
+        return Response({
+            'message': _('OTP is sent. You can request a new one after {} seconds').format(Conf.OTP_WAITING_PERIOD()),
+            'waiting_seconds': Conf.OTP_WAITING_PERIOD()
+        })
 
 
-class ComfirmRegisterView(APIView):
-    pass
+class ConfirmRegisterView(APIView):
+
+    def post(self, request):
+        serializer = ConfirmRegisterSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = ResidentService.create_resident_profile(**serializer.data)
+        return Response(UserSerializer(user).data)
 
 
 class ResidentTokenObtainPairView(TokenObtainPairView):
