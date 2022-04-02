2681f0bedcbff0ffa6ddcbd68aa62417f0092199
Safwan <s.saigh@puritec.sa>
2022-04-02 22:00:31 +0300

    feat: create post app

--- config/settings/base.py
+++ config/settings/base.py
@@ -75,7 +75,8 @@ LOCAL_APPS = [
 
     "apps.campus",
     "apps.announcement",
-    "apps.event"
+    "apps.event",
+    "apps.post",
 ]
 # https://docs.djangoproject.com/en/dev/ref/settings/#installed-apps
 INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
