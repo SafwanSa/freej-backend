6aaa230588999831b8fa8810e3bbcae1da726e9a
Safwan <s.saigh@puritec.sa>
2022-04-06 22:24:59 +0300

    chore: create dummy post models

--- apps/post/models.py
+++ apps/post/models.py
@@ -1,3 +1,22 @@
+from apps.utility.models import BaseModel
 from django.db import models
+from apps.account.models import User
+from enum import Enum
+from core import utils
+from apps.campus.models import ResidentProfile
 
-# Create your models here.
+
+class Post(BaseModel):
+    title = models.CharField(max_length=200)
+    description = models.TextField(null=True, blank=True)
+    created_by = models.ForeignKey(ResidentProfile, on_delete=models.CASCADE, related_name='created_posts')
+    # TODO: images =
+
+    class Meta:
+        abstract = True
+
+
+class Offer(Post):
+    rating = models.FloatField(default=0)
+    num_of_raters = models.IntegerField(default=0)
+    is_active = models.BooleanField(default=True)
