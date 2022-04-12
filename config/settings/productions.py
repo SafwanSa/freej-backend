01d0270924579969b53dbae4408c35b68ab3c055
Safwan <s.saigh@puritec.sa>
2022-04-12 23:05:29 +0300

    fix: add ec2 to allowed hosts

--- config/settings/productions.py
+++ config/settings/productions.py
@@ -9,7 +9,7 @@ DEBUG = True
 # https://docs.djangoproject.com/en/dev/ref/settings/#secret-key
 SECRET_KEY = env("DJANGO_SECRET_KEY")
 # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', '*']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'freejprod-env.eba-iq9mchj3.eu-west-1.elasticbeanstalk.com', '*']
 
 # DATABASES
 # ------------------------------------------------------------------------------
