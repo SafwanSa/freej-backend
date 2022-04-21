8cc009dcfa726c3fdd6c489595f826b76d2a098a
Safwan <s.saigh@puritec.sa>
2022-04-21 06:17:37 +0300

    feat: get application status with post

--- apps/post/serializers.py
+++ apps/post/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from .models import *
+from . import queries
 
 
 class OwnerSerializer(serializers.ModelSerializer):
@@ -28,12 +29,21 @@ class ReviewSerializer(serializers.ModelSerializer):
 
 class PostSerializer(serializers.ModelSerializer):
     owner = OwnerSerializer()
-
     reviews = ReviewSerializer(many=True)
+    application_status = serializers.SerializerMethodField()
 
     class Meta:
         model = Post
         fields = '__all__'
+
+    def get_application_status(self, obj):
+        if self.context.get('show_application_status'):
+            resident_profile = self.context.get('resident_profile')
+            applications = queries.get_all_post_applications_by(beneficiary=resident_profile, post=obj)
+            if applications.exists():
+                application = applications.first()
+                return application.status
+        return None
 
 
 class CreatePostSerializer(serializers.Serializer):
