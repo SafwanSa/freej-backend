bf5202b55e66184e8ea3205b487e1a3123f2a269
Safwan <s.saigh@puritec.sa>
2022-04-21 20:37:02 +0300

    enh: enhance post admin views

--- apps/post/admin.py
+++ apps/post/admin.py
@@ -1,7 +1,65 @@
 from django.contrib import admin
 from .models import *
+from django.utils.translation import gettext_lazy as _
+import nested_admin
+from core import utils
 
 
-admin.site.register(Post)
-admin.site.register(Application)
-admin.site.register(Review)
+class PostAdmin(nested_admin.NestedModelAdmin):
+    class ApplicationInline(nested_admin.NestedTabularInline):
+        model = Application
+        extra = 0
+        fields = ['beneficiary', 'status', 'status_updated_at']
+        readonly_fields = ['status_updated_at']
+
+    class ReviewInline(nested_admin.NestedTabularInline):
+        model = Review
+        extra = 0
+        fields = ['reviewer', 'rating', 'comment']
+
+    model = Post
+    inlines = [ApplicationInline, ReviewInline]
+    list_display = [
+        'id',
+        utils.linkify_field('campus'),
+        utils.linkify_field('owner'),
+        'title',
+        'type',
+        'is_active',
+        'created_at'
+    ]
+    list_filter = ['campus', 'type', 'is_active', 'created_at']
+    search_fields = ['owner__user__username', 'campus__name']
+
+
+class ApplicationAdmin(nested_admin.NestedModelAdmin):
+    model = Application
+    list_display = [
+        'id',
+        utils.linkify_field('post'),
+        utils.linkify_field('beneficiary'),
+        'status',
+        'status_updated_at',
+        'created_at'
+    ]
+    list_filter = ['post__campus', 'status', 'status_updated_at', 'created_at']
+    search_fields = ['beneficiary__user__username', 'post__campus__name']
+
+
+class ReviewAdmin(nested_admin.NestedModelAdmin):
+    model = Review
+    list_display = [
+        'id',
+        utils.linkify_field('post'),
+        utils.linkify_field('reviewer'),
+        'rating',
+        'comment',
+        'created_at'
+    ]
+    list_filter = ['post__campus', 'rating', 'created_at']
+    search_fields = ['reviewer__user__username', 'post__campus__name']
+
+
+admin.site.register(Post, PostAdmin)
+admin.site.register(Application, ApplicationAdmin)
+admin.site.register(Review, ReviewAdmin)
