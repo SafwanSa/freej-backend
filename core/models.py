52a5b5a3b19e18729d25601787b85dccb8f7c6fb
Safwan <s.saigh@puritec.sa>
2022-04-23 22:06:16 +0300

    enh: enahnce soft delete mechanism

--- core/models.py
+++ core/models.py
@@ -1,6 +1,21 @@
 from django.db import models
 
 
+class SoftDeleteManager(models.Manager):
+    def __init__(self, *args, **kwargs):
+        self.with_deleted = kwargs.pop('deleted', False)
+        super(SoftDeleteManager, self).__init__(*args, **kwargs)
+
+    def _base_queryset(self):
+        return super().get_queryset().filter(deleted_at=None)
+
+    def get_queryset(self):
+        qs = self._base_queryset()
+        if self.with_deleted:
+            return qs
+        return qs.filter(is_deleted=False)
+
+
 class BaseModel(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     modified_at = models.DateTimeField(auto_now=True)
@@ -9,3 +24,14 @@ class BaseModel(models.Model):
 
     class Meta:
         abstract = True
+
+    objects = SoftDeleteManager()
+    objects_with_deleted = SoftDeleteManager(deleted=True)
+
+    def delete(self):
+        self.is_deleted = True
+        self.save()
+
+    def restore(self):
+        self.is_deleted = False
+        self.save()
