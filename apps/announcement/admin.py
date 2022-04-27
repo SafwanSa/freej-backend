c544a394bdd9d3115131ea133e0683562f16e1f4
Safwan <s.saigh@puritec.sa>
2022-04-27 20:55:49 +0300

    enh: enhance admin panel

--- apps/announcement/admin.py
+++ apps/announcement/admin.py
@@ -1,32 +1,32 @@
 from django.contrib import admin
 from .models import *
 from django.utils.translation import gettext_lazy as _
-import nested_admin
 from core import utils
+from core.admin import BaseAdmin, BaseStackedInline, BaseTabularInline
 
 
-class AnnouncementAdmin(admin.ModelAdmin):
+class AnnouncementAdmin(BaseAdmin):
     model = Announcement
     list_display = ['id', utils.linkify_field('sender'), 'type', 'title', 'created_at']
     list_filter = ['type', 'created_at']
     search_fields = ['sender__username', 'title']
 
 
-class BuildingAnnouncementAdmin(admin.ModelAdmin):
+class BuildingAnnouncementAdmin(BaseAdmin):
     model = BuildingAnnouncement
     list_display = ['id', utils.linkify_field('sender'), utils.linkify_field('building'), 'title', 'created_at']
     list_filter = ['building', 'created_at']
     search_fields = ['sender__username', 'title']
 
 
-class CampusAnnouncementAdmin(admin.ModelAdmin):
+class CampusAnnouncementAdmin(BaseAdmin):
     model = CampusAnnouncement
     list_display = ['id', utils.linkify_field('sender'), utils.linkify_field('campus'), 'title', 'created_at']
     list_filter = ['campus', 'created_at']
     search_fields = ['sender__username', 'title']
 
 
-class CommercialAnnouncementAdmin(admin.ModelAdmin):
+class CommercialAnnouncementAdmin(BaseAdmin):
     model = CommercialAnnouncement
     list_display = [
         'id',
