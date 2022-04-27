cf548a5c3f60eb5ed558375f3e1e86b3d34400a5
Safwan <s.saigh@puritec.sa>
2022-04-27 22:39:59 +0300

    feat: implement editing building profile

--- apps/campus/urls.py
+++ apps/campus/urls.py
@@ -4,7 +4,9 @@ from . import views
 urlpatterns = [
     path('', views.CampusesView.as_view()),
     path('<int:campus_id>/buildings/', views.BuildingsView.as_view()),
+
     path('residents/me/', views.ResidentProfileView.as_view()),
+    path('residents/me/building/', views.EditBuildingView.as_view()),
     path('residents/me/building-issues/', views.BuildingIssuesView.as_view()),
     path('residents/me/fix-building-issues/<int:pk>/', views.FixBuildingIssuesView.as_view()),
 
