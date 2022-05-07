12b931a79244255910e5bc0631ca1c8df09e248e
Safwan <s.saigh@puritec.sa>
2022-05-07 21:03:38 +0300

    fix: remve re_path import

--- config/urls.py
+++ config/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.conf import settings
-from django.urls import include, path, re_path
+from django.urls import include, path
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
